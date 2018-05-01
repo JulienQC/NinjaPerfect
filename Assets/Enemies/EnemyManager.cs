c7e761461a656d5274cf15914cd6e8404806b11c
JulienQC <julien.cusset33@gmail.com>
2018-05-01 17:12:36 -0400

    Enemies appear and disappear

---
+++ Assets/Enemies/EnemyManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+    public float spawnInterval;
+
+	// Use this for initialization
+	void Start () {
+        if (enemyPrefab == null) return;
+        InvokeRepeating("Spawn", 0, spawnInterval);
+	}
+	
+	void Spawn()
+    {
+        Instantiate(enemyPrefab, transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Scripts/NPC/HealthManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class HealthManager : MonoBehaviour {
+
+    public int lifePoints;
+
+    private delegate void CollisionHandler(GameObject collision);
+    private CollisionHandler m_handler;
+
+    private void Start()
+    {
+        if (gameObject.CompareTag("Player"))
+            m_handler = PlayerCollisionHandler;
+        else
+            m_handler = EnemyCollisionHandler;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        --lifePoints;
+        m_handler(collision.gameObject);
+    }
+
+    private void PlayerCollisionHandler(GameObject collision)
+    {
+
+    }
+
+    private void EnemyCollisionHandler(GameObject collision)
+    {
+        if (lifePoints == 0)
+            Destroy(gameObject);
+    }
+}
